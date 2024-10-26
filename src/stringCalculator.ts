14db32cd55dce4ce93d4c782e283b96c972e3468
gallianoms <ggallianoms@gmail.com>
2024-10-26 15:52:51 +0200

    TCR: Tests passed

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -2,10 +2,12 @@ export const add = (str: string) => {
   if (!str) return 0
   if (str.length === 1) return Number(str)
 
-  const arrStr = str.split(',')
-  let sum = 0
-  for (let i = 0; i < arrStr.length; i++) {
-    sum += Number(arrStr[i])
-  }
-  return sum
+  // const arrStr = str.split(',')
+  // let sum = 0
+  // for (let i = 0; i < arrStr.length; i++) {
+  //   sum += Number(arrStr[i])
+  // }
+  // return sum
+
+  return str.split(',').reduce((acc, val) => acc + Number(val), 0)
 }
