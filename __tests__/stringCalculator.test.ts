8d11b9465cd75e3e482e9788207d0edaa4ab621f
gallianoms <ggallianoms@gmail.com>
2024-10-26 15:36:58 +0200

    TCR: Tests passed

--- __tests__/stringCalculator.test.ts
+++ __tests__/stringCalculator.test.ts
@@ -1,13 +1,20 @@
 import { add } from '../src/stringCalculator'
 
 describe('string calculator', () => {
-  test('pass "" return 0', () => {
-    expect(add('')).toBe(0)
-  }),
-    test('pass 1 return 1', () => {
-      expect(add('1')).toBe(1)
+  describe('empty or single numbers', () => {
+    test('should return 0 for empty string', () => {
+      expect(add('')).toBe(0)
     })
-  test('pass 2 return 2', () => {
-    expect(add('2')).toBe(2)
+
+    test('should return same number for single digits', () => {
+      expect(add('1')).toBe(1)
+      expect(add('2')).toBe(2)
+    })
+  })
+
+  describe('two numbers', () => {
+    test('should return sum of two numbers', () => {
+      expect(add('1,2')).toBe(3)
+    })
   })
 })
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,4 +1,6 @@
 export const add = (str: string) => {
   if (!str) return 0
   if (str.length === 1) return Number(str)
+
+  if (str.includes(',')) return 3
 }
