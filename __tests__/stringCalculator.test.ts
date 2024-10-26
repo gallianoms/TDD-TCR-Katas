cbc89c2d1d60460ee39eeaa4f8e5fc1d2556c13e
gallianoms <ggallianoms@gmail.com>
2024-10-26 15:14:44 +0200

    TCR: Tests passed

--- __tests__/stringCalculator.test.ts
+++ __tests__/stringCalculator.test.ts
@@ -7,4 +7,7 @@ describe('string calculator', () => {
     test('pass 1 return 1', () => {
       expect(add('1')).toBe(1)
     })
+  test('pass 2 return 2', () => {
+    expect(add('1')).toBe(1)
+  })
 })
