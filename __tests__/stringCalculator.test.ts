d337648da7b5a5c354da5bbca93df4284a0703cf
gallianoms <ggallianoms@gmail.com>
2024-10-27 12:00:12 +0100

    TCR: Tests passed

--- __tests__/stringCalculator.test.ts
+++ __tests__/stringCalculator.test.ts
@@ -5,19 +5,19 @@ describe('string calculator', () => {
     test('should return 0 for empty string', () => {
       expect(add('')).toBe(0)
     })
+
+    test('should return same number for single digits', () => {
+      expect(add('1')).toBe(1)
+      expect(add('2')).toBe(2)
+    })
   })
 
-  test('should return same number for single digits', () => {
-    expect(add('1')).toBe(1)
-    expect(add('2')).toBe(2)
+  describe('two numbers', () => {
+    test('should return sum of two numbers', () => {
+      expect(add('1,2')).toBe(3)
+      expect(add('3,4')).toBe(7)
+    })
   })
-
-  // describe('two numbers', () => {
-  //   test('should return sum of two numbers', () => {
-  //     expect(add('1,2')).toBe(3)
-  //     expect(add('3,4')).toBe(7)
-  //   })
-  // })
 
   // describe('multiple numbers', () => {
   //   test('should return sum of multiple numbers', () => {
