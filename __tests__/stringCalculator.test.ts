d28ce471377999c3055b74f4b2bb1d4994762c94
gallianoms <ggallianoms@gmail.com>
2024-10-27 11:59:30 +0100

    TCR: Tests passed

--- __tests__/stringCalculator.test.ts
+++ __tests__/stringCalculator.test.ts
@@ -5,33 +5,33 @@ describe('string calculator', () => {
     test('should return 0 for empty string', () => {
       expect(add('')).toBe(0)
     })
-
-    test('should return same number for single digits', () => {
-      expect(add('1')).toBe(1)
-      expect(add('2')).toBe(2)
-    })
   })
 
-  describe('two numbers', () => {
-    test('should return sum of two numbers', () => {
-      expect(add('1,2')).toBe(3)
-      expect(add('3,4')).toBe(7)
-    })
+  test('should return same number for single digits', () => {
+    expect(add('1')).toBe(1)
+    expect(add('2')).toBe(2)
   })
 
-  describe('multiple numbers', () => {
-    test('should return sum of multiple numbers', () => {
-      expect(add('1,2,3')).toBe(6)
-      expect(add('3,4,5')).toBe(12)
-    })
-  })
+  // describe('two numbers', () => {
+  //   test('should return sum of two numbers', () => {
+  //     expect(add('1,2')).toBe(3)
+  //     expect(add('3,4')).toBe(7)
+  //   })
+  // })
 
-  describe('jump lines', () => {
-    test('should return sum of multiple numbers with jump lines', () => {
-      expect(add('1\n2,3')).toBe(6)
-      expect(add('1\n2,\n3')).toBe(6)
-    })
-  })
+  // describe('multiple numbers', () => {
+  //   test('should return sum of multiple numbers', () => {
+  //     expect(add('1,2,3')).toBe(6)
+  //     expect(add('3,4,5')).toBe(12)
+  //   })
+  // })
+
+  // describe('jump lines', () => {
+  //   test('should return sum of multiple numbers with jump lines', () => {
+  //     expect(add('1\n2,3')).toBe(6)
+  //     expect(add('1\n2,\n3')).toBe(6)
+  //   })
+  // })
 
   // describe('throw exception', () => {
   //   test('should throw exception for negative numbers and show numbers on exception message', () => {
@@ -54,5 +54,4 @@ describe('string calculator', () => {
   //     expect(add('//[***]\n1***2***3')).toBe(6)
   //     expect(add('//[--]\n1--2')).toBe(3)
   //   })
-  // })
 })
--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,39 +1,14 @@
 export const add = (str: string) => {
   if (!str) return 0
-  // if (str.length === 1) return Number(str)
 
-  return str
-    .split('')
+  const numbers = str
+    .split(',')
     .map((val) => Number(val))
     .filter((val) => !isNaN(val))
-    .reduce((acc, num) => (acc += num), 0)
 
-  // if (str === '//[***]\n1***2***3') {
-  //   return 6
-  // }
+  const negatives = numbers.filter((num) => num < 0)
+  if (negatives.length > 0)
+    throw new Error('no allowed negative numbers: ' + negatives)
 
-  // if (str === '//[--]\n1--2') {
-  //   return 3
-  // }
-
-  // let negativeNums = ''
-  // let start = str.indexOf('[') + 1
-  // let end = str.indexOf(']')
-
-  // let delimiter = str.substring(start, end)
-
-  // str = str.slice(end + 2)
-
-  // const replace = str.replace('\n', ',').replace(delimiter, ',')
-  // const split = replace.split(',')
-
-  // negativeNums += split.filter((num) => Number(num) < 0)
-  // if (negativeNums)
-  //   throw new Error('no allowed negative numbers: ' + negativeNums)
-
-  // const result = split
-  //   .filter((num) => Number(num) <= 1000)
-  //   .reduce((acc, val) => acc + Number(val), 0)
-
-  // return result
+  return numbers.reduce((acc, num) => (acc += num), 0)
 }
