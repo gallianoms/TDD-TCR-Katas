3287010a8f52a0ab5902a500490a2b618c495357
gallianoms <ggallianoms@gmail.com>
2024-10-27 20:23:13 +0100

    TCR: Tests passed

--- src/stringCalulator/processNumbers.ts
+++ src/stringCalulator/processNumbers.ts
@@ -1,8 +1,15 @@
+const MAX_ALLOWED_NUMBER = 1000
+const LINE_SEPARATOR = '\n'
+const VALUE_SEPARATOR = ','
+
+const isValidNumber = (val: number): boolean => !isNaN(val)
+const isWithinLimit = (num: number): boolean => num <= MAX_ALLOWED_NUMBER
+
 export function processNumbers(str: string): number[] {
     return str
-        .replaceAll('\n', ',')
-        .split(',')
-        .map((val) => Number(val))
-        .filter((val) => !isNaN(val))
-        .filter((num) => num <= 1000)
+        .replaceAll(LINE_SEPARATOR, VALUE_SEPARATOR)
+        .split(VALUE_SEPARATOR)
+        .map(Number)
+        .filter(isValidNumber)
+        .filter(isWithinLimit)
 }
