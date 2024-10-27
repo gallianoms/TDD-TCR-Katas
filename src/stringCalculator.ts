5b6e1eccddac22294f9d7a549a86e9079ad06859
gallianoms <ggallianoms@gmail.com>
2024-10-27 13:05:16 +0100

    TCR: Tests passed

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,12 +1,35 @@
-export const add = (str: string) => {
+export const add = (str: string): number => {
   if (!str) return 0
+  if (str.length === 1) return Number(str)
 
-  const numbers = str
-    .replace('\n', ',')
+  let start: number
+  let end: number
+  let delimiter: string = ''
+  let numbers: number[] = []
+
+  if (str.includes('[') && str.includes(']')) {
+    start = str.indexOf('[') + 1
+    end = str.indexOf(']')
+    delimiter = str.substring(start, end)
+  }
+
+  numbers = str
+    .replaceAll('\n', ',')
     .split(',')
     .map((val) => Number(val))
     .filter((val) => !isNaN(val))
     .filter((num) => num <= 1000)
+
+  if (delimiter.length > 0) {
+    numbers = str
+      .replaceAll('\n', ',')
+      .split(',')[1]
+      .replaceAll(delimiter, ',')
+      .split(',')
+      .map((val) => Number(val))
+      .filter((val) => !isNaN(val))
+      .filter((num) => num <= 1000)
+  }
 
   const negatives = numbers.filter((num) => num < 0)
   if (negatives.length > 0)
